dd0f60efa6a3c36083b7b7375ca90ecc2c0d2c63
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-13 15:20:27 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-env browser */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
